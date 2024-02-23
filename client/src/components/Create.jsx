259b25a7be3c989853b7b8adc72afc876aa5e7c9
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-02-23 17:54:56 -0300

    updated Create.jsx logic

--- client/src/components/Create.jsx
+++ client/src/components/Create.jsx
@@ -1,10 +1,17 @@
+import { useState } from "react"
+import axios from 'axios'
 
 
 const Create = () => {
+  const [task, setTask] = useState()
+  const handleAdd = () => {
+
+  }
+
   return (
-    <div>
-      <input  type="text" name="" id="" />
-      <button type="button">Add</button>
+    <div className="create_form">
+      <input  type="text" placeholder="Enter Task" onChange={(e) => setTask(e.target.value)} />
+      <button type="button" onClick={handleAdd}>Add</button>
     </div>
   )
 }
