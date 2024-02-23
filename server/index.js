9d61de7bb63c6fab0a94e78e2712d1a9a65bd85a
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-02-23 18:18:17 -0300

    added more logic to index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const TodoModel = require('./Models/Todo')
 
 const app = express()
 app.use(cors())
@@ -10,6 +11,10 @@ mongoose.connect('mongodb+srv://dpodev11:baba-0110@cluster0.7cizlg2.mongodb.net/
 
 app.post('/add', (req, res) => {
   const task = req.body.task;
+  TodoModel.create({
+    task: task
+  }).then(result => res.json(result))
+  .catch(err => res.json(err))
 })
 
 app.listen(3001, () => {
