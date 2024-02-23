e2019e9cdb93cdab5a83c0fca11a2abaa2270853
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-02-23 18:07:22 -0300

    fixed bracket syntax error

--- server/index.js
+++ server/index.js
@@ -8,10 +8,9 @@ app.use(express.json())
 
 mongoose.connect('mongodb+srv://dpodev11:baba-0110@cluster0.7cizlg2.mongodb.net/')
 
-
-app.post('/add', (req, res => {
+app.post('/add', (req, res) => {
   const task = req.body.task;
-}))
+})
 
 app.listen(3001, () => {
   console.log("Server is Running")
