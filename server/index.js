8030901016425f99769d0598c4e111bc63938506
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-02-23 18:03:28 -0300

    updated index.js logic

--- server/index.js
+++ server/index.js
@@ -6,6 +6,12 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+mongoose.connect('mongodb+srv://dpodev11:baba-0110@cluster0.7cizlg2.mongodb.net/')
+
+
+app.post('/add', (req, res => {
+  const task = req.body.task;
+}))
 
 app.listen(3001, () => {
   console.log("Server is Running")
